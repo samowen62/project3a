78b610d9bdd0dc8c6c3e5a2bdf9721865ebafbaf
Philip Tibbetts <tibbetts@galapagos-02.cs.wisc.edu>
2014-10-24 21:16:07 -0500

    final

--- mem.c
+++ mem.c
@@ -8,105 +8,71 @@
 #include <pthread.h>
 #include "mem.h"
 
-/* this structure serves as the header for each block */
 typedef struct block_hd{
-  /* The blocks are maintained as a linked list */
-  /* The blocks are ordered in the increasing order of addresses */
-  struct block_hd* next;
-
-  /* size of the block is always a multiple of 4 */
-  /* ie, last two bits are always zero - can be used to store other information*/
-  /* LSB = 0 => free block */
-  /* LSB = 1 => allocated/busy block */
-
-  /* For free block, block size = size_status */
-  /* For an allocated block, block size = size_status - 1 */
-  /* The size of the block stored here is not the real size of the block */
-  /* the size stored here = (size of block) - (size of header) */
-  short int size_status;
-
+	struct block_hd* next;
+	int size_status;
 }block_header;
 
-/* Global variable - This will always point to the first block */
-/* ie, the block with the lowest address */
 block_header* list_head = NULL;
-static int allocated_once = 0;
+static int uno_alloc = 0;
 
-pthread_mutex_t lock;
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
-/* Function used to Initialize the memory allocator */
-/* Not intended to be called more than once by a program */
-/* Argument - sizeOfRegion: Specifies the size of the chunk which needs to be allocated */
-/* Returns 0 on success and -1 on failure */
-int Mem_Init(int sizeOfRegion)
+int Mem_Init(int size)
 {
-  int pagesize;
-  int padsize;
-  int fd;
-  short int alloc_size;
-  void* space_ptr;
+	int psize;
+ 	int puffer;
+	int fd;
+	short int alloc_size;
+ 	void* space_ptr;
   
- if(0 != allocated_once)
-  {
-    fprintf(stderr,"Error:mem.c: Mem_Init has allocated space during a previous call\n");
-    return -1;
-  }
-  if(sizeOfRegion <= 0)
-  {
-    fprintf(stderr,"Error:mem.c: Requested block size is not positive\n");
-    return -1;
-  }
-  if (pthread_mutex_init(&lock, NULL) != 0)
-  {
-    fprintf(stderr,"Error:mutex init failed\n");
-    return -1;
-  }	
+ 	if(0 != uno_alloc)
+  	{
+    		fprintf(stderr,"Error:mem.c: Mem_Init has allocated space during a previous call\n");
+    		return -1;
+  	}
+  	if(size <= 0)
+  	{
+    		fprintf(stderr,"Error:mem.c: Requested block size is not positive\n");
+    		return -1;
+  	}
+  	if (pthread_mutex_init(&lock, NULL) != 0)
+  	{
+    		fprintf(stderr,"Error:mutex init failed\n");
+    		return -1;
+  	}	
 
-  pagesize = getpagesize();
+  	psize = getpagesize();
+  	puffer = size % psize;
+  	puffer = (psize - puffer) % psize;
+  	alloc_size = size + puffer;
 
-  padsize = sizeOfRegion % pagesize;
-  padsize = (pagesize - padsize) % pagesize;
-
-  alloc_size = sizeOfRegion + padsize;
-
-  fd = open("/dev/zero", O_RDWR);
-  if(-1 == fd)
-  {
-    fprintf(stderr,"Error:mem.c: Cannot open /dev/zero\n");
-    return -1;
-  }
-  space_ptr = mmap(NULL, alloc_size, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0);
-  if (MAP_FAILED == space_ptr)
-  {
-    fprintf(stderr,"Error:mem.c: mmap cannot allocate space\n");
-   allocated_once = 0;
-    return -1;
-  }
+  	fd = open("/dev/zero", O_RDWR);
+  	if(-1 == fd)
+  	{
+    		fprintf(stderr,"Error:mem.c: Cannot open /dev/zero\n");
+    		return -1;
+  	}
+  	space_ptr = mmap(NULL, alloc_size, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0);
+  	if (MAP_FAILED == space_ptr)
+  	{
+    		fprintf(stderr,"Error:mem.c: mmap cannot allocate space\n");
+    		uno_alloc = 0;
+    		return -1;
+  	}
   
-  allocated_once = 1;
+ 	 uno_alloc = 1;
   
-  /* To begin with, there is only one big, free block */
-  list_head = (block_header*)space_ptr;
-  list_head->next = NULL;
-  /* Remember that the 'size' stored in block size excludes the space for the header */
-  list_head->size_status = alloc_size - (int)sizeof(block_header);
+  	list_head = (block_header*)space_ptr;
+  	list_head->next = NULL;
+  	list_head->size_status = alloc_size - (int)sizeof(block_header);
   
-  return 0;
+  	return 0;
 }
 
 
-/* Function for allocating 'size' bytes. */
-/* Returns address of allocated block on success */
-/* Returns NULL on failure */
-/* Here is what this function should accomplish */
-/* - Check for sanity of size - Return NULL when appropriate */
-/* - Round up size to a multiple of 4 */
-/* - Traverse the list of blocks and allocate the first free block which can accommodate the requested size */
-/* -- Also, when allocating a block - split it into two blocks when possible */
-/* Tips: Be careful with pointer arithmetic */
 void* Mem_Alloc(int size)
 {
-
 	block_header* temp = list_head;
 	block_header* addr;
 	short int mem_left; 
@@ -115,8 +81,8 @@ void* Mem_Alloc(int size)
  	
 	if ((size % 8) != 0)
 		size = size - (size % 8) + 8;
-	
-	pthread_mutex_lock(&lock);
+
+	pthread_mutex_lock(&lock);	
 	while (temp != NULL) {
 		mem_left = temp->size_status - size - h_size; 
 		// size of new free block
@@ -146,18 +112,8 @@ void* Mem_Alloc(int size)
 	return NULL;
 }
 
-/* Function for freeing up a previously allocated block */
-/* Argument - ptr: Address of the block to be freed up */
-/* Returns 0 on success */
-/* Returns -1 on failure */
-/* Here is what this function should accomplish */
-/* - Return -1 if ptr is NULL */
-/* - Return -1 if ptr is not pointing to the first byte of a busy block */
-/* - Mark the block as free */
-/* - Coalesce if one or both of the immediate neighbours are free */
-int Mem_Free(void *ptr)
+int Mem_Free(void* ptr)
 {
-	
 	if (ptr == NULL)
 		return -1;
 
@@ -165,8 +121,8 @@ int Mem_Free(void *ptr)
 	block_header* temp = list_head;
 	int h_size = (int)sizeof(block_header);
 
-	// looks if first block is the one we're trying to free
 	pthread_mutex_lock(&lock);
+	// looks if first block is the one we're trying to free
 	if (temp + h_size == ptr)
  	{
 		if (temp->next != NULL) {
@@ -222,29 +178,21 @@ int Mem_Free(void *ptr)
 
 int
 Mem_Available(){
-	int most = 0;
+	pthread_mutex_lock(&lock);
+	int total = 0;
 	block_header* temp = list_head;
 
 	while (temp != NULL) {
 		if(!(temp->size_status & 1))
-			if (temp->size_status >= most)
-				most = temp->size_status;
+		total += temp->size_status;
 	}
 
-	return most;
+	pthread_mutex_unlock(&lock);
+	return total;
 } 
 
-
-/* Function to be used for debug */
-/* Prints out a list of all the blocks along with the followinginforormation for each block */
-/* No.      : Serial number of the block */
-/* Status   : free/busy */
-/* Begin    : Address of the first useful byte in the block */
-/* End      : Address of the last byte in the block */
-/* Size     : Size of the block (excluding the header) */
-/* t_Size   : Size of the block (including the header) */
-/* t_Begin  : Address of the first byte in the block (this is where the header starts) */
-void Mem_Dump() {
+void 
+Mem_Dump() {
 	block_header* temp = list_head;
 
 	while (temp != NULL){
